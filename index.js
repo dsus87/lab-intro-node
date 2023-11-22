bacc2a3232ecd0c8e4a3881b4cd3e726d8f79254
Daniel Susetio <dsusetio87@gmail.com>
2023-11-22 21:17:22 +0100

    iterations 1-3

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items =[];
+    this.length = 0;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b); // sort in ascending order
+    this.length = this.items.length; // update length property
+  }
+
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error('OutOfBounds')
+    }
+    return this.items[pos];
+  }
 
   max() {}
 
@@ -14,6 +27,7 @@ class SortedList {
   avg() {}
 }
 
+
+
 module.exports = SortedList;
 
-//test
