9449271a73c1ab580e5bee6deba0c76f99dc287e
Daniel Susetio <dsusetio87@gmail.com>
2023-11-23 14:38:48 +0100

    iterations 1-5

--- index.js
+++ index.js
@@ -18,9 +18,20 @@ class SortedList {
     return this.items[pos];
   }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length - 1];
 
-  min() {}
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
 
   sum() {}
 
